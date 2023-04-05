09afc001b723b9e8b21a309c173ad8c64cbab538
Gabriel Landry <gablandry31@gmail.com>
2023-04-04 23:03:37 -0400

    🤠 Toutes les positions relatives sont maintenant mise en place.

--- King.cpp
+++ King.cpp
@@ -1,7 +1,8 @@
 #include "King.hpp"
 std::list<Location> King::getPossiblePositions() const
 {
-	return std::list<Location>();
+	// Les positions ont été reprises de mon ancien jeu d'échec en python : https://github.com/Sportek/chess
+	return { {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1} };
 }
 
 void King::display(std::ostream& out) const {
--- Knight.cpp
+++ Knight.cpp
@@ -1,7 +1,8 @@
 #include "Knight.hpp"
 std::list<Location> Knight::getPossiblePositions() const
 {
-	return std::list<Location>();
+	// Les positions ont été reprises de mon ancien jeu d'échec en python : https://github.com/Sportek/chess
+	return { {1, -2}, {2, -1},{2, 1},{1, 2},{-1, 2},{ -2, 1},{ -2, -1},{ -1, -2} };
 }
 
 void Knight::display(std::ostream& out) const {
